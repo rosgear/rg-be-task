52cc4391ff548e656dae750235ab7781c1660e5a
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 16:29:58 +0300

    Module

---
+++ src/Module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Модуль веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\Task;
+
+/**
+ * Модуль системных задач.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\Task
+ * @since 1.0
+ */
+class Module extends \Ge\Panel\Module\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $id = 'rg.be.task';
+}
