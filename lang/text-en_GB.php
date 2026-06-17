cb813ef1e1d2e7dfb9cf233c44dd3868a2c12d6c
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 16:29:24 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'System tasks',
+    '{description}' => 'Executing tasks on the client initiated by the server',
+    '{permissions}' => []
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Системные задачи',
+    '{description}' => 'Выполнение задач на клиенте, инициированных сервером',
+    '{permissions}' => []
+];
